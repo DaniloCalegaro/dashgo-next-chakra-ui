941eae76719af69b9e012b9935b21ea6b25853d6
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-07-06 10:39:39 -0300

    feat: criando form de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-const Home: NextPage = () => {
+const SignIn: NextPage = () => {
   return (
-    <h1>Dashgo</h1>
+    <Flex 
+      w='100vw'
+      h='100vh'
+      align='center'
+      justify='center'
+    >
+      <Flex
+        as='form'
+        width='100%'
+        maxWidth={360}
+        bg='gray.800'
+        p='8'
+        borderRadius={8}
+        flexDir='column'
+      >
+        <Stack spacing='4'>
+          <Input name='email' type='email' label='E-mail' />
+          <Input name='password' type='password' label='Senha' />
+        </Stack>
+        <Button type='submit' mt='6' colorScheme='pink' size='lg'>Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
 
-export default Home
+export default SignIn
