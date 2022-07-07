407c7b066b657549db641fbe6fccee2e8d86952a
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-07-07 13:01:36 -0300

    feat: criado sidebar responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,14 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+export function Profile({ showProfileData = true }) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Danilo Calegaro</Text>
-        <Text color='gray.300' fontSize='small'>danilo.calegaro@hotmail.com</Text>
-      </Box>
+      { showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Danilo Calegaro</Text>
+          <Text color='gray.300' fontSize='small'>danilo.calegaro@hotmail.com</Text>
+        </Box>
+      )}
 
       <Avatar size='md' name='Danilo Calegaro' src='https://github.com/DaniloCalegaro.png' />
     </Flex>
