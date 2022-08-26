7e4e44b3a419795f7669470d264721ebc3b5a4ec
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-08-26 17:21:52 -0300

    feat: configurando miragejs

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,85 +1,108 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
-import Link from "next/link";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { Header } from "../../components/Header";
-import { Pagination } from "../../components/Pagination";
-import { SiderBar } from "../../components/SideBar";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue
+} from '@chakra-ui/react'
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { Header } from '../../components/Header'
+import { Pagination } from '../../components/Pagination'
+import { SiderBar } from '../../components/SideBar'
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
-    lg: true,
+    lg: true
   })
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
 
   return (
     <Box>
       <Header />
 
-      <Flex w='100%' maxWidth={1400} my='6' mx='auto' px='6'>
+      <Flex w="100%" maxWidth={1400} my="6" mx="auto" px="6">
         <SiderBar />
 
-        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
-          <Flex mb='8' justify='space-between' alignItems='center'>
-            <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" alignItems="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
 
-            <Link href='/users/create' passHref>
-              <Button 
-                as='a'
-                size='sm'
-                fontSize='sm'
-                colorScheme='pink'
-                leftIcon={<Icon as={RiAddLine} fontSize='20'/>}
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
               >
                 Criar novo
               </Button>
             </Link>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
+          <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink'/>
+                <Th px={['4', '4', '6']} color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de cadastro</Th>}
-                { isWideVersion && <Th w='8'></Th> }
+                {isWideVersion && <Th>Data de cadastro</Th>}
+                {isWideVersion && <Th w="8"></Th>}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
                 <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink'/>
+                  <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
-                    <Text fontWeight='bold'>Danilo Calegaro</Text>
-                    <Text fontSize='sm' color='gray.300'>danilo.calegaro@hotmail.com</Text>
+                    <Text fontWeight="bold">Danilo Calegaro</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      danilo.calegaro@hotmail.com
+                    </Text>
                   </Box>
                 </Td>
-                { isWideVersion && <Td>04 de Maio, 2022</Td>}
+                {isWideVersion && <Td>04 de Maio, 2022</Td>}
 
-                { isWideVersion && 
+                {isWideVersion && (
                   <Td>
-                    <Button 
-                      as='a'
-                      size='sm'
-                      fontSize='sm'
-                      colorScheme='purple'
-                      leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                     >
                       Editar
                     </Button>
                   </Td>
-                }
-
-                
+                )}
               </Tr>
             </Tbody>
           </Table>
 
           <Pagination />
-
         </Box>
       </Flex>
     </Box>
